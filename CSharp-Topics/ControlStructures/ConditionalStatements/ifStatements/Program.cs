ff1788706ff645913a7dec48c80c1885c87b3f08
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-07-28 15:18:15 +0530

    Add ConditionalStatement?

---
+++ CSharp-Topics/ControlStructures/ConditionalStatements/ifStatements/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConditionalIfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if statement
+            Console.WriteLine("if statement:");
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+
+            // if-else statement
+            Console.WriteLine("if-else statement:");
+            int number = 3;
+
+            if(number > 5)
+            {
+                Console.WriteLine("The number is greater than 5");
+            }
+            else
+            {
+                Console.WriteLine("The number is 5 or less");
+            }
+
+            // else if
+            Console.WriteLine("else if statement:");
+            int num = 10;
+
+            if (num > 10)
+            {
+                Console.WriteLine("The number is greater than 10.");
+            }
+            else if (num == 10)
+            {
+                Console.WriteLine("The number is exactly 10.");
+            }
+            else if (num > 5)
+            {
+                Console.WriteLine("The number is greater than 5 but less than 10.");
+            }
+            else
+            {
+                Console.WriteLine("The number is 5 or less.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp-Topics/ControlStructures/ConditionalStatements/switchStatement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SwitchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("The number is one.");
+                    break;
+                case 2:
+                    Console.WriteLine("The number is two.");
+                    break;
+                case 3:
+                    Console.WriteLine("The number is three.");
+                    break;
+                case 4:
+                    Console.WriteLine("The number is four.");
+                    break;
+                default:
+                    Console.WriteLine("The number is not between 1 and 4.");
+                    break;
+            }
+
+            Console.WriteLine("Enhanced Example With Fall-through :");
+            char grade = 'B';
+
+            switch (grade)
+            {
+                case 'A':
+                case 'B':
+                case 'C':
+                    Console.WriteLine("Passing grade.");
+                    break;
+                case 'D':
+                case 'F':
+                    Console.WriteLine("Failing grade.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
