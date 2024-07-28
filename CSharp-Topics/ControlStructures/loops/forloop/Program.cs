1fe711e19bdb5b90bc2dcaca7e63acacb6058c40
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-07-28 21:26:25 +0530

    Add Loops? for loop & neated loop

--- CSharp-Topics/ControlStructures/loops/forloop/Program.cs
+++ CSharp-Topics/ControlStructures/loops/forloop/Program.cs
@@ -15,6 +15,19 @@ namespace forLoop
             {
                 Console.WriteLine(i);
             }
+
+            Console.WriteLine("\n\nnested for loop");
+            // Outer loop
+            for (int i = 1; i <= 2; ++i)
+            {
+                Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+                // Inner loop
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(" Inner: " + j);  // Executes 6 times (2 * 3)
+                }
+            }
             Console.ReadLine();
         }
     }
--- CSharp-Topics/README.md
+++ CSharp-Topics/README.md
@@ -868,3 +868,27 @@ O/p:
 ```
 
 ## Nested Loops:
+```csharp
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Outer loop
+      for (int i = 1; i <= 2; ++i) 
+      {
+        Console.WriteLine("Outer: " + i);  // Executes 2 times
+        
+        // Inner loop
+        for (int j = 1; j <= 3; j++) 
+        {
+          Console.WriteLine(" Inner: " + j);  // Executes 6 times (2 * 3)
+        }
+      }    
+    }
+  }
+}
+```
