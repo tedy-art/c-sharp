a4c9d31ee37be282d37981e1726e1f8f37822825
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-08-02 19:14:20 +0530

    Add OOP? multiple objects and classes?

---
+++ CSharp-Topics/OOP/Multpleclasses/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
---
+++ CSharp-Topics/OOP/MultpleclassesObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultipleClassesAndObjects
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car MyCarObject1 = new Car();
+            Car MyCarObject2 = new Car();
+            Console.WriteLine(MyCarObject1.color);
+            Console.WriteLine(MyCarObject2.color);
+            Console.ReadLine();
+        }
+    }
+}
