84137ec53fde5889dd0b28ac9bced8d2fb7f9d2f
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-08-05 11:16:02 +0530

    Add OOP? Access Modifiers?

---
+++ CSharp-Topics/OOP/Constructors/createConstructorWithParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyConstructorParameterApplication
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+
+        // creating a class constructor with parameter
+        public Car(string modelName, string ModelColor, int modelYear)
+        {
+            model = modelName;
+            // we can use as many parameter as we want
+            color = ModelColor;
+            year = modelYear;
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("mustang", "Red", 1969);
+            Console.WriteLine($"{Ford.color} {Ford.year} {Ford.model}.");
+            Console.ReadLine();
+        }
+    }
+}
